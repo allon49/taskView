3182e54c6ce00099c4b423e0b1d3a6d03ffe3685
Emmanuel Charruau <echarruau@gmail.com>
2020-05-28 12:38:39 +0200

    added connection test

--- tache.js
+++ tache.js
@@ -2,6 +2,7 @@
 .import QtQuick 2.6 as Quick
 .import GCompris 1.0 as GCompris
 
+
 var items
 
 var tasks = [["tache bleue 1","tache bleue 2","tache bleue 3","tache bleue 4","tache bleue 5","tache bleue 6","tache bleue 7","tache bleue 8","tache bleue 9","tache bleue 10"],
@@ -16,28 +17,69 @@ function start(items_) {
 }
 
 
+var component
+var currentTaskColumn
+
 function init() {
 
-    for (var i = 0; i < tasks.length; i++) {
+    console.log("init")
 
-        var component = Qt.createComponent("qrc:/gcompris/src/activities/taskView/tasksInfo.qml");
-        var currentTaskColumn = component.createObject(
+    component = Qt.createComponent("qrc:/gcompris/src/activities/taskView/tasksInfo.qml");
+
+    if (component.status === Quick.Component.Ready) {
+        console.log("do")
+        finishCreation();
+    }
+    else {
+        console.log("wait")
+        component.statusChanged.connect(finishCreation);
+    }
+}
+
+
+
+function finishCreation() {
+    if (component.status === Quick.Component.Ready) {
+        currentTaskColumn = component.createObject(
                        items.root,
                        {
                            "header": "testHeader",
                            "tasks": "testTask",
                            "footer": "testFooter"
                        });
+        if (currentTaskColumn === null) {
+            // Error Handling
+            console.log("Error creating object");
+        }
 
-        items.tasksModel.append(currentTaskColumn)
+        console.log("eee")
 
+
+    } else if (component.status === Quick.Component.Error) {
+        // Error Handling
+        console.log("Error loading component:", component.errorString());
+    }
+
+    items.tasksModel.append(currentTaskColumn)
+
+    items.taskRepeater.model = tasksModel
+
+    console.log("rr")
+
+
+}
+
+
+
+//    for (var i = 0; i < tasks.length; i++) {
+//        items.tasksModel.append(currentTaskColumn)
 
 //        for (var j = 0; j < tasks[i].length; j++) {
 //            console.log("---" + tasks[i][j])
 //        }
-    }
+//    }
 
 
 
 
-}
+
